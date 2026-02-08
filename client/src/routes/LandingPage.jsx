f7ef078737f8b88bdea206db9cabe39d0adf3cbd
Anishka Chokshi <anishka.chokshi@gmail.com>
2026-02-07 19:57:59 -0600

    added about me blurb

--- client/src/routes/LandingPage.jsx
+++ client/src/routes/LandingPage.jsx
@@ -6,7 +6,8 @@ export default function LandingPage() {
     <div style={{ padding: "2rem", maxWidth: 600, margin: "0 auto" }}>
       <h1>Tutor Match</h1>
       <p>
-        Find freelance tutors based on subject, age group, city/ZIP, and hourly rate.
+        Welcome to TutorMatch! 
+        Find freelance tutors in your area based on subject, age group, and hourly rate.
       </p>
       <div style={{ marginTop: "1.5rem", display: "flex", gap: "1rem" }}>
         <Link to="/register">
@@ -16,6 +17,18 @@ export default function LandingPage() {
           <button>Log in</button>
         </Link>
       </div>
+      <h1>About Us</h1>
+      <p>
+        After experiencing inefficient and disconnected communication in the freelance tutoring space, 
+        Audrey Bolyard, Rashi Sharma, and Anishka Chokshi founded TutorMatch: a one-stop shop for tutors and students alike.
+        
+        Our site offers a forum for students seeking help and tutors offering service, bolstering local community in the process. 
+        Students can quickly find tutors that fit their needs, budget, and schedule, while tutors can connect with students who are interested.
+
+        Our goal is simple: make tutoring more transparent, efficient, and connective— 
+        because finding the right tutor shouldn’t feel harder than the subject itself.
+      </p>
+
     </div>
   );
 }
